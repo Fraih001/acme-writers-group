ebc9cbd08d6b1679a94b33e4c6d0d0c8d4803eb5
fraih001 <fraiha26@gmail.com>
2022-06-29 10:44:40 -0400

    re-factored Story component

--- src/Story.js
+++ src/Story.js
@@ -6,7 +6,7 @@ class Story extends Component{
     constructor(){
       super();
       this.state = {
-        story: {}
+        story: {},
       };
 
       this.favoriteAStory = this.favoriteAStory.bind(this);
@@ -18,48 +18,48 @@ async componentDidMount(){
     this.setState({ story: response.data });
 }
 
-// async componentDidUpdate(prevProps){
-//     if(prevProps.story.favorite !== this.props.story.favorite){
-//         let response = await axios.get(`/api/users/${this.props.story.userId}/stories/${this.props.story.id}`);
-//         this.setState({ story: response.data });
-//     }
-//   };
+async componentDidUpdate(prevProps){
+    if(prevProps.story !== this.props.story){
+        let response = await axios.get(`/api/users/${this.props.story.userId}/stories/${this.props.story.id}`);
+        this.setState({ story: response.data });
+    }
+  };
 
-async favoriteAStory(userId, story){
-    let response = await favoriteStory(userId, story);
-    console.log(response.data)
-    this.setState({ story: response.data });
+async favoriteAStory(userId, storyId){
+    let story = await favoriteStory(userId, storyId);
+    this.setState({ story });
+    console.log(this.state.story.favorite)
  
   }
 
-  async unfavoriteAStory(userId, story){
-    let response = await unfavoriteStory(userId, story)
-    this.setState({ story: response.data })
+  async unfavoriteAStory(userId, storyId){
+    let story = await unfavoriteStory(userId, storyId)
+    this.setState({ story });
+    console.log(this.state.story.favorite)
   }
 
 render(){ 
     const { unfavoriteAStory, favoriteAStory } = this;
     const { story } = this.state;
-    // console.log(this.state.story.favorite)
-    // console.log(this.props.story);
+
     return(
         
-        <li key={ this.props.story.id }>
-                  { this.props.story.title }
-                  <p>
-                  { this.props.story.body }
-                  </p>
-                  <button onClick = { () => this.props.deleteAStory(this.props.story)} >DELETE</button>
+            <li key={ story.id }>
+                        { story.title }
+                        <p>
+                        { story.body }
+                        </p>
+                        <button onClick = { () => this.props.deleteAStory(story)} >DELETE</button>
 
-                  { this.props.story.favorite ? 
-                  <button id="unfavorite" onClick = { () => { unfavoriteAStory(this.props.story.userId, this.props.story.id) }}>Un-Favorite</button> :
-                  <button id="favorite" onClick = { () => { favoriteAStory(this.props.story.userId, this.props.story.id); }}>Favorite</button>
-                  }   
-                 
-                </li>
+                        {/* { this.props.story.favorite ?  */}
+                        <button id="unfavorite" onClick = { () => { unfavoriteAStory(story.userId, story.id) }}>Un-Favorite</button> 
+                        <button id="favorite" onClick = { () => { favoriteAStory(story.userId, story.id) }}>Favorite</button>
+                        {/* }    */}
+                        
+            </li>
 
-            )
-        }
+        )
+     }
 
 }
 
